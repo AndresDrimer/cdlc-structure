d2df24c6ea605fc44797c2b1aaab82ec300da796
AndresDrimer <andresdrimer@hotmail.com>
2023-07-30 13:23:30 -0300

    harcoded img try

--- app/components/Thumbnail.js
+++ app/components/Thumbnail.js
@@ -12,7 +12,8 @@ const Thumbnail = ({ product }) => {
       className={`relative  min-w-[180px] cursor-pointer transition duration-200 ease-out h-70 md:h-70 md:min-w-[260px] md:hover:scale-105`}
     >
       <img
-      src={product.imgFront}
+      src="https://drive.google.com/uc?export=view&id=14aF7wb2iYhxCTDwhj2UT89aFbbcf1eqA"
+
         className="rounded-sm md:rounded relative "
         alt="imagen de tapa"
         width={200}
