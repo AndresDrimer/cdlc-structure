6d521fe87d440cf197f8b1e15b6d40ba633cb8b6
AndresDrimer <andresdrimer@hotmail.com>
2023-07-30 13:11:56 -0300

    Image try

--- app/components/Thumbnail.js
+++ app/components/Thumbnail.js
@@ -1,7 +1,7 @@
 
-import Image from "next/image";
 import Link from "next/link";
 import ProductDetail from "./ProductDetails";
+import Image from "next/image";
 
 
 const Thumbnail = ({ product }) => {
@@ -11,13 +11,14 @@ const Thumbnail = ({ product }) => {
       href={`/${product._id}`}
       className={`relative  min-w-[180px] cursor-pointer transition duration-200 ease-out h-70 md:h-70 md:min-w-[260px] md:hover:scale-105`}
     >
-      <img
+      <Image
       src={product.imgFront}
         className="rounded-sm md:rounded relative "
         alt="imagen de tapa"
         width={200}
         height={200}
     />
+    
 
     {product.discountPercentage !==0 && (<div className="font-xs px-2 bg-gris text-rojo absolute top-0 right-0 shadow-sm hover:text-white">  {product.discountPercentage}% off </div>) } 
      </Link>
